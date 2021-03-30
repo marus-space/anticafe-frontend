f46432ee0250a30e0a8e7b410424f934fff9290b
Maria Gopkalo <marus.space@gmail.com>
2021-03-30 23:19:23 +0300

    Add MyComponent

---
+++ dreamy-whale-app/src/components/MyComponent/MyComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class MyComponent extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'http://localhost:8000/api/clients/';
+    fetch(apiUrl,{ method: 'GET', headers: { 'Content-Type': 'application/json'}})
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  };
+  render() {
+    return (
+    <div>
+      <h1>Check the browser 'console' </h1>
+    </div>
+    );
+  };
+};
+
+export default MyComponent;
