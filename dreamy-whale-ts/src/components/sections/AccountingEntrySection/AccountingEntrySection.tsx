cd90a0d47e2a552a308942460c9d476e8ba976e0
Maria Gopkalo <marus.space@gmail.com>
2021-06-15 18:56:20 +0300

    Удалила комментарий

--- dreamy-whale-ts/src/components/sections/AccountingEntrySection/AccountingEntrySection.tsx
+++ dreamy-whale-ts/src/components/sections/AccountingEntrySection/AccountingEntrySection.tsx
@@ -47,28 +47,4 @@ const AccountingEntrySection: React.FC = () => {
     );
 };
 
-// class AccountingEntrySection extends React.Component {
-//     state = {
-//         selectedRowKeys: [],
-//     };
-    
-//     onSelect = (selectedRowKeys: React.Key[]) => {
-//         this.setState({ selectedRowKeys });
-//         console.log(this.state.selectedRowKeys);
-//     };
-    
-//     render () {
-//         const { Content } = Layout;
-//         return (
-//             <div className={classes.component} onClick={() => this.onSelect([])}>
-//                 <Header title="Проводки" />
-//                 <Content className={classes.content}>
-//                     <AccountingEntryActionBar selectedRowKeys={this.state.selectedRowKeys} />
-//                     <AccountingEntryTable selectedRowKeys={this.state.selectedRowKeys} onSelect={this.onSelect} />
-//                 </Content>
-//             </div>
-//         );
-//     }
-// }
-
 export default AccountingEntrySection;
